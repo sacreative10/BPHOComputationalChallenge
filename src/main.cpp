fdd1fc11267cafd9774d5518b93713d47a941e30
Sohaib Alam <sacreative10@gmail.com>
2024-07-04 14:31:09 +0100

    Challenge 1

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,34 @@
 #include <SFML/Window/Event.hpp>
 
 #include "SFML/Graphics/CircleShape.hpp"
+#include "SFML/Graphics/Sprite.hpp"
+#include "SFML/Graphics/Texture.hpp"
 #include <imgui-SFML.h>
 #include <imgui.h>
 
-void controlPanel() {
+#include <math.h>
+#include <vector>
+
+struct point {
+  float x;
+  float y;
+};
+ 
+void controlPanel(float& timestep, float& launchSpeed, float& strengthOfGravity, float& launchHeight, int& launchAngle, float &scale) {
   ImGui::Begin("Hello, world!");
-  ImGui::Text("This is where controls for the simulation go");
-  ImGui::Button("Button");
-  ImGui::End();
+  ImGui::Text("General Settings");
+  ImGui::SliderFloat("Timestep", &timestep, 0.01, 0.02);
+  ImGui::SliderFloat("Strength of Gravity", &strengthOfGravity, 0.1, 10);
+
+  ImGui::Text("Projectile Settings");
+  ImGui::SliderInt("Launch Angle", &launchAngle, 0, 90);
+  ImGui::SliderFloat("Launch Speed", &launchSpeed, 0, 100);
+  ImGui::SliderFloat("Launch Height", &launchHeight, 0, 100);
+  ImGui::SliderFloat("Scale Factor", &scale, 0, 100);
+  ImGui::End(); 
+  
 }
+
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(1280, 720), "BPHO Computational Challenge");
@@ -20,10 +39,6 @@ int main() {
 
   sf::Clock deltaClock;
   while (window.isOpen()) {
-
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-    shape.setPosition(100.f, 100.f);
 
     sf::Event event;
     while (window.pollEvent(event)) {
@@ -36,11 +51,48 @@ int main() {
 
     ImGui::SFML::Update(window, deltaClock.restart());
 
-    controlPanel();
+    static float timestep = 0.01;
+    static float launchSpeed = 20.f;
+    static float strengthOfGravity = 9.81;
+    static float launchHeight = 2;
+    static int launchAngle = 45;
+    static float scale = 1.f;
+
+    controlPanel(timestep, launchSpeed, strengthOfGravity, launchHeight, launchAngle, scale);
+
+    std::vector<point> points;
+
+    float launchAngle_rad = launchAngle * 3.14159f / 180.f;
+    float t = 0;
+    float x;
+    float ux = launchSpeed*std::cos(launchAngle_rad);
+    float uy = launchSpeed*std::sin(launchAngle_rad);
+    float y = launchHeight;
+
+    points.push_back({0, y * scale});
+
+    while (y >= 0){
+      t += timestep;
+      x = ux * t * scale;
+      y = (launchHeight + uy*t -0.5f * strengthOfGravity * t*t) * scale;
+      points.push_back({x, y});       
+    }
+
 
     window.clear();
     ImGui::SFML::Render(window);
-        window.draw(shape);
+    for (point p: points)
+    {
+      sf::CircleShape ball(2.f);
+      ball.setFillColor(sf::Color::Green);
+      ball.setPosition(p.x, 720 - p.y);
+      window.draw(ball);
+    }
+    sf::CircleShape ball(2.f);
+    ball.setFillColor(sf::Color::Red);
+    ball.setPosition(points[0].x, 720 - points[0].y);
+    window.draw(ball);
+
     window.display();
   }
 
